dc5d4c50c941b69e9c10006ced3c587d29ec79f8
JamesACM <jaimecanalesmarin@gmail.com>
2018-03-10 00:02:40 -0300

    add nestes_attributes (category)

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,7 @@
 class Report < ApplicationRecord
   require 'resolv'
-  has_many :categories, dependent: :destroy
+  has_many :categories, dependent: :destroy, inverse_of: :report
   validates_presence_of :isWhitelisted
-  validates :ip_address, :presence => true, :format => { :with => Resolv::IPv4::Regex }
+  validates_presence_of :ip_address, :format => { :with => Resolv::IPv4::Regex }
+  accepts_nested_attributes_for :categories
 end
